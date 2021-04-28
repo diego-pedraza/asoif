112f1be16535a1e2355b8b9e74cc33781d86b8ab
Diego Pedraza <dev.diego.pedraza@gmail.com>
2021-04-29 01:18:24 +0200

    Initial commit (general setup). Missing Filters and some styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import parse from 'parse-link-header';
+import Table from './Table';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const baseUrl = 'https://www.anapioficeandfire.com/api/';
+
+	const [page, setPage] = useState('1');
+	const [pageSize, setPageSize] = useState('10');
+	const [paginationUrls, setPaginationUrls] = useState({});
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState([]);
+
+	const getData = async (finalUrl) => {
+		const response = await fetch(finalUrl);
+		const data = await response.json();
+		setPaginationUrls(await parse(response.headers.get('link')));
+		setData(data);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		const url = `${baseUrl}/characters?page=${page}&pageSize=${pageSize}`;
+		getData(url);
+	}, [page, pageSize]);
+
+	return (
+		<main>
+			<div className='section-title'>
+				<h1>{loading ? 'Loading...' : 'A Song of Ice and Fire: Characters'}</h1>
+				<div className='underline'></div>
+			</div>
+			{/* TODO:
+                Add Section with search bar (let's make it as big as the table) + Results per page?
+                Right bellow add (centered) Results per page
+                Filter by: radio button? (gender)
+             */}
+			<section className='section-center'>
+				<Table characters={data} />
+			</section>
+			<section>
+				{!loading && (
+					<div className='btn-container'>
+						{Object.entries(paginationUrls).map((item) => {
+							return (
+								<button
+									key={item[0]}
+									className='pagination-btn'
+									onClick={() => {
+										setPage(item[1].page);
+									}}
+								>
+									{item[0]}
+								</button>
+							);
+						})}
+					</div>
+				)}
+			</section>
+		</main>
+	);
 }
 
 export default App;
